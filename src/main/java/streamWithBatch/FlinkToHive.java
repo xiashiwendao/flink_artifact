9a0a848a605dae1d56db08c9e570cf0919f9f6af
Lorry <xiashiwendao@163.com>
2020-11-16 14:56:09 +0800

    update

--- src/main/java/streamWithBatch/FlinkToHive.java
+++ src/main/java/streamWithBatch/FlinkToHive.java
@@ -24,8 +24,9 @@ public class FlinkToHive {
 				.build();
 		StreamTableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv, tableEnvSettings);
 		String category_name = "hive_category";
+		String default_DB= "flink2hive";
 		HiveCatalog catalog = new HiveCatalog(category_name, // catalog name
-				"flink2hive", // default database
+				default_DB, // default database
 				"/etc/hive/conf", // Hive config (hive-site.xml) directory
 				"1.2.1" // Hive version（现在只是支持1.2.1和2.3.4两个版本）
 		);
@@ -37,45 +38,54 @@ public class FlinkToHive {
 		tableEnv.useCatalog(category_name);
 		
 		// 创建Kafka表（本质是consumer），用户流计算
-		tableEnv.useDatabase("stream_tmp");
+		tableEnv.useDatabase(default_DB);
 		// 设置方言为默认（使用默认方言创建kafka相关表）
 		tableEnv.getConfig().setSqlDialect(SqlDialect.DEFAULT);
-		String table_name = "analytics_access_log_kafka";
-		String createKafkaLogTbl = " CREATE TABLE analytics_access_log_kafka (\n" + "   ts BIGINT,\n"
-				+ "   userId BIGINT,\n" + "   eventType STRING,\n"
-				+ "   siteId BIGINT,\n" + "   procTime AS PROCTIME(),\n"
-				+ "   eventTime AS TO_TIMESTAMP(FROM_UNIXTIME(ts / 1000,'yyyy-MM-dd HH:mm:ss')),\n"
-				+ "   WATERMARK FOR eventTime AS eventTime - INTERVAL '15' SECOND\n" + " ) WITH (\n"
-				+ "   'connector' = 'kafka',\n" + "   'topic' = 'ods_analytics_access_log',\n"
-				+ "   'properties.bootstrap.servers' = '172.16.103.96:9092',\n"
-				+ "   'properties.zookeeper.connect' = '172.16.103.90:2181',\n"
-				+ "   'properties.group.id' = 'flink_hive_integration_exp_1',\n"
-				+ "   'scan.startup.mode' = 'latest-offset',\n" + "   'format' = 'json',\n"
-				+ "   'json.fail-on-missing-field' = 'false',\n" + "   'json.ignore-parse-errors' = 'true'\n" + " )";
+		String table_name_kafka = "analytics_access_log_kafka_2";
+		String createKafkaLogTbl = "CREATE TABLE " + table_name_kafka +" (\n" + 
+				"   logTime STRING,\n" + 
+				"   message STRING,\n" + 
+				"   eventType STRING,\n" + 
+				"   userId STRING,\n" + 
+				"   siteId STRING,\n" + 
+				"   procTime AS PROCTIME(),\n" + 
+				"   eventTimestamp AS TO_TIMESTAMP(logTime),\n" + 
+				"   WATERMARK FOR eventTimestamp AS eventTimestamp - INTERVAL '15' SECOND\n" + 
+				" ) WITH (\n" + 
+				"   'connector' = 'kafka',\n" + 
+				"   'topic' = 'test_2',\n" + 
+				"   'properties.bootstrap.servers' = 'cdh6:9092',\n" + 
+				"   'properties.group.id' = 'apache_log_flink',\n" + 
+				"   'scan.startup.mode' = 'latest-offset',\n" + 
+				"   'format' = 'json',\n" + 
+				"   'json.fail-on-missing-field' = 'false',\n" + 
+				"   'json.ignore-parse-errors' = 'false'\n" + 
+				" )\n" + 
+				"";
 		
-		ifNotExistCreateTable(tableEnv, table_name, createKafkaLogTbl);
+		ifNotExistCreateTable(tableEnv, table_name_kafka, createKafkaLogTbl);
 		
 		// 创建hive表，用于流批一体中流计算同步到Hive中
 		tableEnv.getConfig().setSqlDialect(SqlDialect.HIVE);
-		table_name = "analytics_access_count";
+		String table_name_hive = "analytics_access_count_2";
 		tableEnv.useDatabase("flink2hive");
 		// 创建Hivetable，用来记录指定时间段内各个url的访问数量(从Flink中同步到此表）
-		String createCountTbl = "CREATE TABLE flink2hive.analytics_access_count (\n" + 
+		String createCountTbl = "CREATE TABLE " + table_name_hive + " (\n" + 
 				"    url STRING,\n" + 
 				"    access_count BIGINT,\n" + 
 				"    start_time TIMESTAMP,\n" + 
 				"    end_time TIMESTAMP\n" + 
-				") STORED AS PARQUET;\n";
-		ifNotExistCreateTable(tableEnv, table_name, createCountTbl);
+				") STORED AS PARQUET\n";
+		ifNotExistCreateTable(tableEnv, table_name_hive, createCountTbl);
 
 		tableEnv.getConfig().setSqlDialect(SqlDialect.HIVE);
-		String insertSql = "INSERT into flink2hive.analytics_access_count\n" + 
-				"    SELECT CAST(siteId as VARCHAR) as siteId\n" + 
-				"    ,count(*) as cnt\n" + 
-				"    ,TUMBLE_START(eventTime, INTERVAL '10' SECOND) as wStar\n" + 
-				"    ,TUMBLE_END(eventTime, INTERVAL '10' SECOND) as wEnd\n" + 
-				"    from stream_tmp.analytics_access_log_kafka\n" + 
-				"    group by TUMBLE(eventTime, INTERVAL '10' SECOND), siteId";
+		String insertSql = "INSERT into " + table_name_hive + " \n" + 
+				"SELECT siteId\n" + 
+				",count(*) as cnt\n" + 
+				",TUMBLE_START(eventTimestamp, INTERVAL '20' SECOND) as wStar\n" + 
+				",TUMBLE_END(eventTimestamp, INTERVAL '20' SECOND) as wEnd\n" + 
+				"from " + table_name_kafka + "\n" + 
+				"group by TUMBLE(eventTimestamp, INTERVAL '20' SECOND), siteId\n";
 		
 		tableEnv.executeSql(insertSql);
 	}
